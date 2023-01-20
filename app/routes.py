71ac71c8f93b8eae3a96a265569fb4f087816629
Tim Hollis <tim.hollis@gmail.com>
2023-01-20 18:33:12 -0500

    templates set up

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'username':'Tim'}
+    posts = [
+        {
+            'author': {'username': 'Kate'},
+            'body': 'Moar tattoos!'
+        },
+        {
+            'author': {'username': 'Darcy'},
+            'body': 'I love to chew things!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
