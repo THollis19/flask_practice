e138ea867d5751f3a32f423ae94519455a46b420
Tim Hollis <tim.hollis@gmail.com>
2023-01-23 19:50:33 -0500

    created sqllite database

--- app/models.py
+++ app/models.py
@@ -2,9 +2,9 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.string(64), index=True, unique=True)
-    email = db.Column(db.String(120, index=True, unique=True))
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(120))
 
     def __repr__(self):
-        reutrn '<User {}>'.format(self.username)
+        return '<User {}>'.format(self.username)
