99c609e592392f4ea80f404401ef43294fa0c63e
Tim Hollis <tim.hollis@gmail.com>
2023-01-25 12:48:27 -0500

    added profile page

--- app/routes.py
+++ app/routes.py
@@ -62,3 +62,13 @@ def register():
         flash('You are now registered!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'I am a test post'},
+        {'author': user, 'body': 'I am also a test post'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
