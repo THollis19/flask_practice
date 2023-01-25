6e0225c5e065f75a77f64d49942c8235bb73d969
Tim Hollis <tim.hollis@gmail.com>
2023-01-25 13:14:38 -0500

    profile pages typos fixed

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db, login
+from hashlib import md5
 
 @login.user_loader
 def load_user(id):
@@ -23,6 +24,11 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -31,3 +37,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
