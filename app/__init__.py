4839a604ad89665ae2ba5ac816bc303de2b46e05
Tim Hollis <tim.hollis@gmail.com>
2023-02-02 13:13:43 -0500

    added flask-moment and times to posts and user profiles

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 import logging, os
 from logging.handlers import SMTPHandler, RotatingFileHandler
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +17,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
