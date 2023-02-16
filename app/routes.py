ab14758e8a7c8316661daa153f6ea35d0d3a5713
Tim Hollis <tim.hollis@gmail.com>
2023-02-16 15:25:04 -0500

    translate to english function

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from werkzeug.urls import url_parse
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
 from app.email import send_password_reset_email
-from app.tranlate import translate
+from app.translate import translate
 from flask_login import current_user, login_user, logout_user, login_required
 from datetime import datetime
 from langdetect import detect, LangDetectException
@@ -179,7 +179,7 @@ def reset_password(token):
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
 
-@app.route('/transate', methods=['POST'])
+@app.route('/translate', methods=['POST'])
 @login_required
 def translate_text():
     return jsonify({'text': translate(request.form['text'], request.form['source_language'], request.form['dest_language'])})
--- app/translate.py
+++ app/translate.py
@@ -10,7 +10,17 @@ def translate (text, source_language, dest_language):
         'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY'],
         'Ocp-Apim-Subscription-Region': 'eastus'
     }
+    print('sending request')
+    print('https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from={}&to={}'.format(source_language, dest_language))
+    print(auth)
+    print(text)
+
     r = requests.post('https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from={}&to={}'.format(source_language, dest_language), headers=auth, json=[{'Text': text}])
     if r.status_code != 200:
+        print(r.status_code)
+        print(r.reason)
+        print(r.headers)
         return ('Error: the translation service failed.')
+    print(r.status_code)
+    print(r.json()[0]['translations'][0]['text'])
     return r.json()[0]['translations'][0]['text']
